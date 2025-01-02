1ad47e91d4f497a548b99d16e9b673f6ba98cc08
Mariia <145922337+mariazhenchur@users.noreply.github.com>
2025-01-02 22:37:58 +0000

    made changes

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,22 +1,21 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_URI);
-
 let cachedClient = null;
 let cachedDb = null;
 
 export const connectToDatabase = async () => {
+    // If there is a cached client and db, return them
     if (cachedClient && cachedDb) {
         return cachedDb;
     }
 
-    if (!client.isConnected()) {
-        await client.connect();
-    }
+    // Create a new MongoClient instance if not already connected
+    const client = new MongoClient(process.env.MONGODB_URI2); // Use your MongoDB URI here
+    await client.connect(); // Wait for the connection to be established
 
+    // Cache the client and database instances
     cachedClient = client;
-    cachedDb = client.db();
+    cachedDb = client.db(); // Use the default database or specify the one you need
 
     return cachedDb;
 };
-
