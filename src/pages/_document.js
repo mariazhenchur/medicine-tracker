88502b21149977fd0d94f85f0c9ea12814e29bc0
Mariia <145922337+mariazhenchur@users.noreply.github.com>
2025-01-06 13:11:01 +0000

    icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Link rel="icon" href="/favicon.png" />
       <body>
         <Main />
         <NextScript />
